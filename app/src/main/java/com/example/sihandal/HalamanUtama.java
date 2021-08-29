eb906fca992c373f324ffcb506eb7a8182f14468
alifya <alifyacode@gmail.com>
2021-08-29 20:23:54 +0700

    splashscreen

---
+++ app/src/main/java/com/example/sihandal/HalamanUtama.java
@@ -0,0 +1,15 @@
+package com.example.sihandal;
+
+import static com.example.sihandal.R.layout.*;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HalamanUtama extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_halaman_utama);
+    }
+}
